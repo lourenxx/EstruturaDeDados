a579fa3e0a65c67b05d4243094df2f58aa0ab1b6
Guilherme Lourenço <guilhermepexiloureco@gmail.com>
2025-03-27 20:03:51 -0300

    adiciona arquivo ponteiro.cpp

---
+++ ponteiro.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+int numero = 42;
+
+// Declarando um ponteiro e atribuindo o endereço de 'numero'
+int* ponteiro = &numero; // ponteiro é um ponteiro para int
+
+std::cout << "Valor de numero: " << numero << std::endl;
+std::cout << "Endereco de numero: " << &numero << std::endl;
+// Desreferenciando o ponteiro10
+std::cout << "Valor do ponteiro: " << *ponteiro << std::endl;
+std::cout << "Endereco do ponteiro: " << ponteiro << std::endl;
+
+// Modificando o valor através do ponteiro
+*ponteiro = 99; // ponteiro aponta para o endereço de 'numero', então *ponteiro modifica 'numero'
+std::cout << "Novo valor de numero: " << numero << std::endl;
+
+return 0;
+}
